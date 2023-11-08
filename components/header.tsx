ef5f303a69d0558fd48dd0e5743500c976563b95
mickasmt <sermont.mickael@hotmail.fr>
2023-11-09 00:04:47 +0100

    docs: fix subtitle

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <div className="mb-5 sm:mb-8 flex flex-col justify-between gap-4 md:flex-row md:items-center">
       <div className="flex items-center gap-4">
         <Image
-          className="h-16 w-16 rounded-full overflow-hidden object-cover"
+          className="h-16 w-16 rounded-full shrink-0 overflow-hidden object-cover"
           src="/avatar.png"
           alt="avatar"
           width={100}
@@ -14,7 +14,9 @@ export function Header() {
 
         <div>
           <h1 className="text-3xl font-bold">mickasmt</h1>
-          <p className="text-muted-foreground">Fullstack Developer | Future Product Manager</p>
+          <p className="text-muted-foreground">
+            Fullstack Dev | Future PM
+          </p>
         </div>
       </div>
     </div>
