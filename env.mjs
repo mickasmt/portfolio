5495d9a7c565762ce71bd939c8b3bc821e4b130b
mickasmt <sermont.mickael@hotmail.fr>
2023-11-08 19:48:10 +0100

    fix: t3 env file

--- env.mjs
+++ env.mjs
@@ -3,10 +3,10 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
+    GITHUB_OAUTH_TOKEN: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
-    GITHUB_OAUTH_TOKEN: z.string().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
